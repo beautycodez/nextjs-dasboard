8a060105efd42b0357dae7cebe1bab7ef1cbfa42
beautycodez <angellorl_94@hotmail.com>
2024-10-09 19:29:37 -0500

    lint issues were fixed with the expection of the  state in edit-form

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -3,6 +3,8 @@ import { z } from "zod";
 import { sql } from "@vercel/postgres";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 
 const FormSchema = z.object({
   id: z.string(),
@@ -61,7 +63,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
   } catch (error) {
     // If a database error occurs, return a more specific error.
     return {
-      message: "Database Error: Failed to Create Invoice.",
+      message: `Database Error: Failed to Create Invoice and ${error}.`
     };
   }
 
@@ -93,7 +95,7 @@ export async function updateInvoice(id: string, prevState: State, formData: Form
           WHERE id = ${id}
         `;
   } catch (error) {
-    return { message: "Database Error: Failed to Update Invoice." };
+    return { message: `Database Error: Failed to update Invoice and ${error}.` };
   }
 
   revalidatePath("/dashboard/invoices");
@@ -106,6 +108,25 @@ export async function deleteInvoice(id: string) {
     revalidatePath("/dashboard/invoices");
     return { message: "Deleted Invoice." };
   } catch (error) {
-    return { message: "Database Error: Failed to Delete Invoice." };
+    return { message: `Database Error: Failed to delete Invoice and ${error}.` };
+  }
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
   }
 }
